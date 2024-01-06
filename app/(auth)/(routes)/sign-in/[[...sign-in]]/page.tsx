b98c7cfc1459cafa1cab25837939fca7bedce278
Nguyễn Trần Trung Hậu <ntthau0963@gmail.com>
2024-01-06 13:45:25 +0700

    Update layout and dependencies

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '@/components/component/login'
+
+const SignInPage = () => {
+  return (
+    <Login />
+  )
+}
+
+export default SignInPage
---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup } from '@/components/component/signup'
+
+const SignUpPage = () => {
+  return (
+    <Signup />
+  )
+}
+
+export default SignUpPage
